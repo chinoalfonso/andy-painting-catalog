6fad1734499f464f96dbf392497f1813d0a61eeb
Juan Alfonso Echevarria Castro <162085821+chinoalfonso@users.noreply.github.com>
2025-06-12 04:20:59 -0400

    Add files via upload

--- assent/js/blog_script.js
+++ assent/js/blog_script.js
@@ -1,38 +1,39 @@
-        // Funcionalidad del modal
-//const modal = document.getElementById('myModal');
-       // const btn = document.querySelector('.open-modal-btn');
-     //   const closeBtn = document.querySelector('.close-btn');
-        
-     //   btn.addEventListener('click', () => {
-     //       modal.style.display = 'flex';
-      //      document.body.style.overflow = 'hidden';
-      //  });
-        
-      //  closeBtn.addEventListener('click', () => {
-      //      modal.style.display = 'none';
-      //      document.body.style.overflow = 'auto';
-       // });
-        
-       // window.addEventListener('click', (e) => {
-     //       if (e.target === modal) {
-     //           modal.style.display = 'none';
-     //           document.body.style.overflow = 'auto';
-     //       }
-     //   });
+               // Funcionalidad del modal
+               const modal2 = document.getElementById('myModal');//id modal
+               const btn = document.querySelector('.open-modal-btn');//boton abrir
+               const closeBtn = document.querySelector('.close-btn');//boton cerrar
+               
+               btn.addEventListener('click', () => {
+                   modal2.style.display = 'flex';
+                   document.body.style.overflow = 'hidden';
+               });
+               
+               closeBtn.addEventListener('click', () => {
+                   modal2.style.display = 'none';
+                   document.body.style.overflow = 'auto';
+               });
+               
+               window.addEventListener('click', (e) => {
+                   if (e.target === modal2) {
+                       modal2.style.display = 'none';
+                       document.body.style.overflow = 'auto';
+                   }
+               });
+       
 
-// Abrir modal
+/* //Abrir modal
 btn.addEventListener('click', () => {
     modal.style.display = 'flex';
-    document.body.classList.add('modal-open');
+   document.body.classList.add('modal');
 });
 
-// Cerrar modal
+ //Cerrar modal
 function closeModal() {
     modal.style.display = 'none';
-    document.body.classList.remove('modal-open');
+   document.body.classList.remove('modal');
 }
 
 closeBtn.addEventListener('click', closeModal);
 window.addEventListener('click', (e) => {
     if (e.target === modal) closeModal();
-});
+});*/
